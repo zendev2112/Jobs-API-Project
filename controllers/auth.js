a44174ec0f5df20d9cb5c5735a6c54ee834b8969
zen <zenon.deviagge@gmail.com>
2024-04-11 15:11:18 -0300

    Create user basic setup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,9 @@
+const { StatusCodes } = require('http-status-codes');
+const User = require('../models/User');
+
 const register = async (req, res) => {
-  res.send('register user');
+  const user = await User.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
