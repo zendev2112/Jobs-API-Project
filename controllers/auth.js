98077894812c1ba179c138a4e4f14de57d8e030e
zen <zenon.deviagge@gmail.com>
2024-04-11 16:00:50 -0300

    Create mongoose middleware on User model. Delete password generation functionality on auth controller.

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,14 +4,7 @@ const User = require('../models/User');
 const { BadRequestError } = require('../errors');
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  const tempUser = { name, email, password: hashedPassword };
-
-  const user = await User.create({ ...tempUser });
+  const user = await User.create({ ...req.body });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -21,6 +22,11 @@ const UserSchema = new mongoose.Schema({
     required: [true, 'Please provide password'],
     minlength: 6,
   },
+});
+
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model('User', UserSchema);
