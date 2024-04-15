192a2b9cbc926633601314beed4a0caf903263e5
zen <zenon.deviagge@gmail.com>
2024-04-15 16:04:51 -0300

    Move JWT token from auth.js controller to UserSchema, using the instance method.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,11 @@
 const { StatusCodes } = require('http-status-codes');
-const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const { BadRequestError } = require('../errors');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  // This code snippet generates a JSON Web Token (JWT) using the jwt.sign function. The token contains the user's ID (userId) and name (name). The token is signed using the secret key 'jwtSecret'.
-  const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', {
-    expiresIn: '30d',
-  });
+  const token = user.createJWT();
+
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
