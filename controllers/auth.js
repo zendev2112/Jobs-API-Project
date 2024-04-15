5cddd5fa9581033863dc2051a686ea1948e637ee
zen <zenon.deviagge@gmail.com>
2024-04-15 15:48:15 -0300

    In the `token` variable generate JWT inside of the register function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,15 @@
 const { StatusCodes } = require('http-status-codes');
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const { BadRequestError } = require('../errors');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  // This code snippet generates a JSON Web Token (JWT) using the jwt.sign function. The token contains the user's ID (userId) and name (name). The token is signed using the secret key 'jwtSecret'.
+  const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', {
+    expiresIn: '30d',
+  });
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
