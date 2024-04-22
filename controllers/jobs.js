3bb2ed4e33408cd06442ae0b48c6672edece67d3
zen <zenon.deviagge@gmail.com>
2024-04-22 20:07:44 -0300

    Create deleteJob controller. Destructure JobId and userId from req obj. Check for errors. Invoke the findByIdAndDelete method to the Job Schema. Return 200 Status.

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -55,7 +55,19 @@ const updateJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-  res.send('delete job');
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findByIdAndDelete({
+    _id: jobId,
+    createdBy: userId,
+  });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).send();
 };
 
 module.exports = {
