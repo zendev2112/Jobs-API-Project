8ef19e4aa1f29c398cca3a5fa632e04b1491be1a
zen <zenon.deviagge@gmail.com>
2024-04-24 18:43:00 -0300

    Add rateLimiter parameters object

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ require('express-async-errors');
 const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
-const rateLimit = require('express-rate-limit');
+const rateLimiter = require('express-rate-limit');
 
 const express = require('express');
 
@@ -23,9 +23,20 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    // store: ... , // Redis, Memcached, etc. See below.
+  }),
+);
 app.use(express.json());
 app.use(helmet());
-app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use('/api/v1/auth', authRouter);
